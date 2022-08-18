0529597e8a3d0f98c3b978f4619dc4fe434fcad0
Nikita <Nikitasmirnov2000@yandex.ru>
2022-08-18 17:59:52 +0300

    ds2

---
+++ Ds2/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Write("Введите число: ");
+int numberN = Convert.ToInt32(Console.ReadLine());
+int FnumberN = numberN;
+int num = 0;
+int sum = 0;
+
+while (numberN > 0)
+{
+    num = numberN % 10;
+    numberN = numberN / 10;
+    sum = sum + num;
+}
+
+Console.WriteLine($"Сумма цифр в числе {FnumberN} = {sum}");
