069afda1e63dc7a3f82957b1258708e54aa18e91
Nikita <Nikitasmirnov2000@yandex.ru>
2022-08-18 17:11:49 +0300

    ds1

---
+++ Ds1/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Write("Введите число: ");
+int Num1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите степень для предыдущего числа: ");
+int Num2 = Convert.ToInt32(Console.ReadLine());
+
+int Numbers(int A, int B)
+{
+    int extentB = 1;
+    for (int i = 0; i < B; i++)
+    {
+        extentB = extentB * A;
+    }
+    return extentB;
+}
+
+int result = Numbers(Num1, Num2);
+Console.WriteLine($"Результат: {Num1}^{Num2} = {result}");
